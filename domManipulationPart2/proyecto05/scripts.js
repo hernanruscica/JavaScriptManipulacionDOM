0a581edd2e8925f4f3d828125a1b1d1747e7c38d
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-08-31 17:43:16 -0300

    Add files via upload
    mayor improvements and refactoring

--- domManipulationPart2/proyecto05/scripts.js
+++ domManipulationPart2/proyecto05/scripts.js
@@ -1,141 +1,118 @@
 
-/*
-Manejo del DOM con JavaScript
-Proyecto numero cuatro:
-*/
+
+/*DOM Elements: */
 const $d = document;
+const $task_add_input = $d.getElementById("task_add_container");
+const $task_add_button = $d.getElementById("task_add_btn");
+//const $tasks = $d.getElementById("tasks");
+/*DOM Elements: */
 
-console.log("desde Jscript");
 
-//los elementos del DOM: 
+/*CONSTANTS */
+let tasksQuantity = 0;
+let taskNumberId = 0;
+const 
+TASK_QUANTITY_MAX = 10,
+ID_MESSAGE_LABEL = 'message',
+DELAY_TIME_INIT = 10000,
+DELAY_TIME_NORMAL = 4000,
+//ENGLISH = 0 SPANISH = 1
+LANGUAGE = 1,
+MESSAGES = [
+            ['You can insert a new task', 'The field [task] can´t be empty.', 'Maximum task limit reached', 'Are you sure you want to delete the following task?', 'Task Deleted', 'To Do list Project', 'Insert a task'],
+            ['Puede ingresar una nueva tarea', 'El campo [Tarea] NO puede estar vacio.', 'Limite de tareas maximas alcanzado', 'Esta seguro de borrar la siguiente tarea?', 'Tarea eliminada', 'Proyecto lista de tareas', 'Ingrese una tarea']                
+            ];
+/*CONSTANTS */
 
-const $inputAgregar = $d.getElementById("agregar_tarea_input");
-const $botonAgregar = $d.getElementById("agregar_tarea_btn");
-const $tareas = $d.getElementById("tareas");
-const $mensaje = $d.getElementById("mensaje");
-
- 
-console.log($inputAgregar);
-console.log($botonAgregar);
-
-let cantidadTareas = 0, CANTIDAD_TAREAS_MAXIMAS = 10;
-let numeroTareaId = 0;
-//texto del mensaje por default
-$mensaje.innerHTML = "Insert a task, then thick it or deleted it";
-
-$botonAgregar.addEventListener("click", () => {
-    //console.log("agregar tarea boton");
-    agregarTarea($inputAgregar.value, numeroTareaId);
+       
+/*EVENTS HANDLERS*/
+$d.addEventListener("DOMContentLoaded", function() {    
+    showMessage(ID_MESSAGE_LABEL, MESSAGES[LANGUAGE][0], DELAY_TIME_NORMAL);
+    writeTitle(MESSAGES[LANGUAGE][5]);
+  });
+$task_add_button.addEventListener("click", () => {
+    //console.log("Add Task Button");
+    addTask($task_add_input.value, taskNumberId);
 });
-
-$inputAgregar.addEventListener("keyup", (e) => {
+$task_add_input.addEventListener("keyup", (e) => {
     //console.log(e.key);
     if (e.key == "Enter"){
         //console.log("agregar tarea por enter en input");
-        agregarTarea($inputAgregar.value, numeroTareaId);
+        addTask($task_add_input.value, taskNumberId);
     }
 });
+/*EVENTS HANDLERS*/
 
 
-/*
-<div class = "lista_tareas_contenedor" id = "tarea_numero_1">
-    <p class="tarea">Collect Shopping Collect Shopping</p>
-    <button class = "hecha"  onclick="tacharTarea('tarea_numero_1')"><i class="fa-solid fa-check"></i></button>
-    <button class = "eliminar" onclick="eliminarTarea('tarea_numero_1')"><i class="fa-solid fa-trash-can"></i></button>
-</div>  
-*/
-const agregarTarea = (textoTarea, tareaNumero) => {
-    
-    if (textoTarea == ""){        
-        mostrarMensaje("El campo [Tarea] NO puede estar vacio.", 2000);
-        return;
-    }
-    if (cantidadTareas >= CANTIDAD_TAREAS_MAXIMAS){
-        mostrarMensaje(`Limite de tareas maximas alcanzado = ${CANTIDAD_TAREAS_MAXIMAS}`, 2000);
-        return;
-    }
-    $inputAgregar.value = "";
-
-    //lista_tareas_contenedor
+/***************  FUNCTIONS TO MANAGE THE TASK ELEMENTS  ********************/
+const createTask = (idTaskContainer, taskText, taskNumber)  =>{
+    //tasks container DOM element
+    const $tasks = $d.getElementById(idTaskContainer);
+    //task list container
     const $divContenedorTareas = $d.createElement("div");    
-    $divContenedorTareas.classList.add("lista_tareas_contenedor");
-    $divContenedorTareas.setAttribute("id", "tarea_numero_" + tareaNumero);
-
-    //parrafo
+    $divContenedorTareas.classList.add("task_list_container");
+    $divContenedorTareas.setAttribute("id", "task_number_" + taskNumber);
+    //paragraph
     const $parrafoTarea = $d.createElement("p");
-    $parrafoTarea.classList.add("tarea");
-    $parrafoTarea.innerHTML = textoTarea;
-    $parrafoTarea.setAttribute("onclick", `tacharTarea("tarea_numero_${tareaNumero}")`);
-
-    //boton de tarea hecha
+    $parrafoTarea.classList.add("task");
+    $parrafoTarea.innerHTML = taskText;
+    $parrafoTarea.setAttribute("onclick", `crossoutTask("task_number_${taskNumber}")`);
+    //task done button
     const $botonHecha = $d.createElement("button");    
-    $botonHecha.classList.add("hecha");
-    $botonHecha.setAttribute("onclick", `tacharTarea("tarea_numero_${tareaNumero}")`);
-
-    //icono de tarea hecha, el tilde o check
+    $botonHecha.classList.add("done");
+    $botonHecha.setAttribute("onclick", `crossoutTask("task_number_${taskNumber}")`);
+    //task done icon
     const $iconoChequeado = $d.createElement("i");
     $iconoChequeado.classList.add("fa-solid");
     $iconoChequeado.classList.add("fa-check");
-
-    //Boton de eliminar tarea
+    //delete task button
     const $botonEliminar = $d.createElement("button");    
-    $botonEliminar.classList.add("eliminar");
-    //$botonEliminar.setAttribute("onclick", `mostrar("tarea_numero_${tareaNumero}")`); 
-    $botonEliminar.setAttribute("onclick", `generarYmostrarModal("tarea_numero_${tareaNumero}")`); 
-
-     //icono de eliminar 1tarea, el tachito de basura
+    $botonEliminar.classList.add("delete");    
+    $botonEliminar.setAttribute("onclick", `createShowModal("task_number_${taskNumber}")`); 
+     //delete task icon, the trash can
      const $iconoEliminar = $d.createElement("i");
      $iconoEliminar.classList.add("fa-solid");
      $iconoEliminar.classList.add("fa-trash-can");
-
-     //agrego los hijos de los botones, los iconos
+     //add the icons as a button's child
      $botonHecha.appendChild($iconoChequeado);
-     $botonEliminar.appendChild($iconoEliminar);
-     
-     //agrego los hijos del div contenedor
+     $botonEliminar.appendChild($iconoEliminar);     
+     //add the children of the div container
      $divContenedorTareas.appendChild($parrafoTarea);
      $divContenedorTareas.appendChild($botonHecha);
      $divContenedorTareas.appendChild($botonEliminar);
-
-     //agrego al div tareas, originalmente en el HTML
-     $tareas.appendChild($divContenedorTareas);
-
-     cantidadTareas += 1;
-     numeroTareaId += 1;
+     //add all to the tasks div
+     $tasks.insertAdjacentElement('afterbegin', $divContenedorTareas);
+}
+const addTask = (taskText, taskNumber) => {    
+    if (taskText == ""){                
+        showMessage(ID_MESSAGE_LABEL, MESSAGES[LANGUAGE][1], DELAY_TIME_NORMAL);
+        return;
+    }
+    if (tasksQuantity >= TASK_QUANTITY_MAX){        
+        showMessage(ID_MESSAGE_LABEL, MESSAGES[LANGUAGE][2], DELAY_TIME_NORMAL);
+        return;
+    }
+    $task_add_input.value = "";
+    createTask('tasks', taskText, taskNumber);
+    tasksQuantity += 1;
+    taskNumberId += 1;
 };
-//agregarTarea("primer tarea", 1);
-//agregarTarea("otra tarea", 2);
-
-
-const eliminarTarea = (idTarea) => {
-    ocultarModal();    
-    const $tareaAeliminar = $d.getElementById(idTarea);    
-    $tareaAeliminar.remove();
-    cantidadTareas -= 1;
-    mostrarMensaje("SE ELIMINO LA TAREA", 2000);
-    //console.log("eliminando tarea ", $tareaAeliminar);
-    
+const deleteTask = (taskId) => {
+    hideModal();    
+    const $taskToDelete = $d.getElementById(taskId);    
+    $taskToDelete.remove();
+    tasksQuantity -= 1;
+    showMessage(ID_MESSAGE_LABEL, MESSAGES[LANGUAGE][4], DELAY_TIME_NORMAL);    
 }
-//eliminarTarea("tarea_numero_1");
-
-const tacharTarea = (idTarea) => {
-    $parrafoTarea = $d.getElementById(idTarea).firstElementChild;
-    //console.log($parrafoTarea);
-    $parrafoTarea.classList.toggle("tachada");
+const crossoutTask = (taskId) => {
+    $taskParagraph = $d.getElementById(taskId).firstElementChild;    
+    $taskParagraph.classList.toggle("through");
 }
+/***************  FUNCTIONS TO MANAGE THE TASK ELEMENTS  ********************/
 
-const generarYmostrarModal = (idTarea) => {
-    /*
-    <div class="modal">
-        <p>Mensaje del modal</p>
-        <div id="botones_confirmacion"> 
-            <button onclick = "eliminarTarea('tarea_numero_1')">si</button>
-            <button onclick = "ocultarModal()">no</button>
-        </div>        
-    </div>
-    */
 
-    $d.querySelector(".bloqueo_pantalla").setAttribute("style", "display:block;");
+/***********FUNCTIONS TO MANAGE THE MODAL CONFIRMATION*********************/
+const createShowModal = (taskId) => {   
    //Si ya existe un modal, lo elimino para tener uno nuevo con el id de tarea correcto
    const $modalBuscado = $d.querySelector(".modal");
     if ( $modalBuscado != null){ 
@@ -144,58 +121,58 @@ const generarYmostrarModal = (idTarea) => {
     //Si no existe un modal, lo creo y lo muestro
     else {
         //consigo el texto de la tarea
-        let textoTarea = $d.getElementById(idTarea).firstElementChild.innerHTML;
-        //console.log("este es el mensaje: ", textoTarea);
-
+        let taskText = $d.getElementById(taskId).firstElementChild.innerHTML;        
         //creo el modal que es un div
         const $modal = $d.createElement("div");
         $modal.classList.add("modal");
-
+        //creo el modal que es un div
+        const $window = $d.createElement("div");
+        $window.classList.add("window");
         //creo el parrafo del mensaje del modal
         const $mensajeModal = $d.createElement("p");
-        $mensajeModal.innerHTML = `Are you sure you want to delete the following task?: <br> "${textoTarea}"`;
-
+        $mensajeModal.innerHTML = `${MESSAGES[LANGUAGE][3]}: <br> "${taskText}"`;
         //creo el contenedor de los botones
         const $botonesContenedor = $d.createElement("div");
         $botonesContenedor.setAttribute("id", "botones_confirmacion");
-
         //creo los botones de confirmacion y le asigno las acciones del evento onclick
         const $botonSi = $d.createElement("button");
-        $botonSi.setAttribute("onclick", `eliminarTarea('${idTarea}')`);
+        $botonSi.setAttribute("onclick", `deleteTask('${taskId}')`);
         $botonSi.innerHTML = "YES";
-
         const $botonNo = $d.createElement("button");
-        $botonNo.setAttribute("onclick", "ocultarModal();");
+        $botonNo.setAttribute("onclick", "hideModal();");
         $botonNo.innerHTML = "NO";
-
         $botonesContenedor.appendChild($botonSi);
         $botonesContenedor.appendChild($botonNo);
-
-        $modal.appendChild($mensajeModal);
-        $modal.appendChild($botonesContenedor);
-
-        $d.body.appendChild($modal);
-        //$d.body.appendChild(modalText);
-        
+        $window.appendChild($mensajeModal);
+        $window.appendChild($botonesContenedor);
+        $modal.appendChild($window);
+        $d.body.appendChild($modal); 
     }
 }
-
-
-const ocultarModal = () => {
-    const $modal = $d.querySelector(".modal");
-    //console.log($modal);
-    $modal.remove();
-    $d.querySelector(".bloqueo_pantalla").setAttribute("style", "display:none;");
+const hideModal = () => {
+    const $modal = $d.querySelector(".modal");    
+    $modal.remove();    
 }
-//mostrarMensaje("SE ELIMINO LA TAREA", 2000);
-const mostrarMensaje = (mensajeAmostrar, tiempoAmostrar) => {
-    let mensajeAnterior = $mensaje.innerHTML;
-    $mensaje.innerHTML = mensajeAmostrar;
-    setInterval(() => {
-        $mensaje.innerHTML =  mensajeAnterior;
-    }, tiempoAmostrar);
+/***********FUNCTIONS TO MANAGE THE MODAL CONFIRMATION*********************/
+
+
+/*showMessage function */
+const showMessage = (idMessageLabel, messageShow, timeShow) => {
+    const $message = $d.getElementById(idMessageLabel);
+    $message.innerHTML = "";
+    $message.innerHTML = messageShow;
+    let showedMessageInterval;
+    if (!showedMessageInterval) {
+        showedMessageInterval = setInterval(() => {
+            $message.innerHTML = "";  
+            clearInterval(showedMessageInterval);
+            showedMessageInterval = null;          
+        }, timeShow);
+    } 
 }
 
-
-
-
+const writeTitle = (text) => {
+    $d.getElementById('header_title').innerHTML = "";
+    $d.getElementById('header_title').innerHTML = text;
+    $d.getElementById('task_add_container').setAttribute('placeholder', MESSAGES[LANGUAGE][6]);    
+};
